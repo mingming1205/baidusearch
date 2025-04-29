baea521ecc206b7b3200e80530878c2d1ed13217
fengye <zhengchenhappy@163.com>
2025-04-29 15:35:09 +0800

    Update baidu_page.py
    删除注释

--- baidu_page.py
+++ baidu_page.py
@@ -8,7 +8,6 @@ class BasePage:
     def open(self,url):
         self.driver.get(url)
 
-# 继承父类BasePage
 class BaiduPage(BasePage):
 
     @property
@@ -19,7 +18,6 @@ class BaiduPage(BasePage):
     def search_button(self):
         return self.driver.find_element(By.ID,'su')
 
-# 另一个页面也需要用到初始化打开网页，则考虑用父类-BasePage
 class MailPage(BasePage):
 
     def account_page(self):
