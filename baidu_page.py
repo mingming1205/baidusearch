5873f9ea308001a5e02adf3876cf3ff4bfd9624e
fengye <zhengchenhappy@163.com>
2025-04-30 10:08:10 +0800

    Update baidu_page.py

--- baidu_page.py
+++ baidu_page.py
@@ -1,10 +1,12 @@
 from selenium.webdriver.common.by import By
+
 class BasePage:
     def __init__(self,driver):
         self.driver = driver
 
     def open(self,url):
         self.driver.get(url)
+        time.sleep(1)
 
 class BaiduPage(BasePage):
 
