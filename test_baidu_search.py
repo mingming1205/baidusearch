4723ed3dce93062c10c6eca3f2e8ad77fc9cfc59
fengye <zhengchenhappy@163.com>
2025-05-09 12:38:56 +0800

    Update test_baidu_search.py

--- test_baidu_search.py
+++ test_baidu_search.py
@@ -9,6 +9,7 @@ class TestBaidu(unittest.TestCase):
 
     def setUp(self):
         self.dr = webdriver.Chrome()
+        # self.dr = webdriver.Edge()
         self.url = "https://www.baidu.com"
         self.dr.maximize_window()
 
@@ -16,7 +17,6 @@ class TestBaidu(unittest.TestCase):
         self.dr.quit()
 
     def test_search(self):
-
         page = BaiduPage(self.dr)
 
         page.open(self.url)
@@ -30,15 +30,19 @@ if __name__ == '__main__':
     test_dir = './'
     suit = unittest.defaultTestLoader.discover(test_dir, pattern='test_*.py')
 
+    # 获取当前日期和时间
     now_time = time.strftime("%Y%m%d_%H%M%S", time.localtime(time.time()))
 
     html_report = './test_report/' + now_time + 'report.html'
+    fp = open(html_report, 'wb')
 
     runner = HTMLTestRunner(stream=fp,
-                            title="HTMLTestReport",
+                            title="HTMLReport",
                             description="运行环境：Windows11，Chrome浏览器"
                             )
     runner.run(suit)
 
+    # 结果写入文件后关闭
+    fp.close()
 
 
