4dcc1420232915b4bb5f54bda0de2ff062b76301
fengye <zhengchenhappy@163.com>
2025-05-09 22:20:18 +0800

    Update test_baidu_search.py

--- test_baidu_search.py
+++ test_baidu_search.py
@@ -4,12 +4,10 @@ from selenium import webdriver
 from HTMLTestRunner import HTMLTestRunner
 from baidu_page import BaiduPage
 
-
 class TestBaidu(unittest.TestCase):
 
     def setUp(self):
         self.dr = webdriver.Chrome()
-        # self.dr = webdriver.Edge()
         self.url = "https://www.baidu.com"
         self.dr.maximize_window()
 
@@ -21,16 +19,17 @@ class TestBaidu(unittest.TestCase):
 
         page.open(self.url)
 
+        # 类对象的方法
+        # 由在baidu_page中加装饰器@property,可把操作对象search_input()小括号去掉
         page.search_input.send_keys("page object")
         page.search_button.click()
-        time.sleep(1)
+        time.sleep(2)
 
 
 if __name__ == '__main__':
     test_dir = './'
     suit = unittest.defaultTestLoader.discover(test_dir, pattern='test_*.py')
 
-    # 获取当前日期和时间
     now_time = time.strftime("%Y%m%d_%H%M%S", time.localtime(time.time()))
 
     html_report = './test_report/' + now_time + 'report.html'
@@ -42,7 +41,7 @@ if __name__ == '__main__':
                             )
     runner.run(suit)
 
-    # 结果写入文件后关闭
     fp.close()
+
 
 
